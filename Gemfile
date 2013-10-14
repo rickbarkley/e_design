881c820372969fed7237287a75e344a247b370c1
Rick Barkley <rick_barkley@nuvola-networks.com>
2013-10-13 22:17:24 -0500

    update

--- Gemfile
+++ Gemfile
@@ -12,7 +12,6 @@ gem "aws-sdk"
 gem "devise"
 gem "cancan"
 gem "wicked_pdf"
-gem "rmagick"
 gem "ransack"
 gem 'rolify'
 
--- db/migrate/20131001155937_add_attachment_to_plan.rb
+++
@@ -1,7 +0,0 @@
-class AddAttachmentToPlan < ActiveRecord::Migration
-  def change
-    add_attachment :plans, :drawing_1
-    add_attachment :plans, :drawing_2
-    add_attachment :plans, :elevation
-  end
-end
